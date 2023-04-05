610e19603a81a7f52e983d5eb65e5af6162ec182
Reptartheman <117610334+Reptartheman@users.noreply.github.com>
2023-04-05 17:30:30 -0400

    Update user-routes.js
    FInished the user routes

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -3,7 +3,15 @@ const { User } = require('../../models');
 
 
 /* 
-    This POST request will create a new user
+    This POST request will create a new user in the database.
+    - .create() will create a new user.
+    - req.session.save() will save the users session data into
+    the server. 
+    - .save() will is called on the req.session object and
+    will make sure the session is saved before the res is sent
+    to the user.
+    - user_id and logged_in properties are set in the session 
+    object to show that the user is logged in.
 
 */
 router.post('/', async (req, res) => {
@@ -20,3 +28,61 @@ router.post('/', async (req, res) => {
       res.status(400).json(err);
     }
   });
+
+
+/* This POST request is using the /login endpoint and using an asynchronous function
+to find some user data.
+  - Using the findOne() method we are using a where clause to find the username in the
+  req.body object.
+  - If that username is not entered into the login correctly, we will get the client error.
+  - It will also check for a valid password passing the req.body object as a parameter.
+  - If that password is not correct, we will get the client error.
+  - req.session.save() will save the session to the server.
+  - then an error if something goes wrong.
+*/
+ router.post("/login", async (req, res) => {
+    try {
+      const userData = await User.findOne({ where: { name: req.body.name } });
+  
+      if (!userData) {
+        res
+          .status(400)
+          .json({ message: "Incorrect username or password, please try again" });
+        return;
+      }
+  
+      const validPassword = await userData.checkPassword(req.body.password);
+  
+      if (!validPassword) {
+        res
+          .status(400)
+          .json({ message: "Incorrect username or password, please try again" });
+        return;
+      }
+  
+      req.session.save(() => {
+        req.session.user_id = userData.id;
+        req.session.logged_in = true;
+  
+        res.json({ user: userData, message: "You are now logged in!" });
+      });
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  });
+
+/* 
+   Logout and end the session ONLY if the user is logged in.
+*/
+
+  router.post("/logout", (req, res) => {
+    if (req.session.logged_in) {
+      req.session.destroy(() => {
+        res.status(204).end();
+      });
+    } else {
+      res.status(404).end();
+    }
+  });
+  
+  module.exports = router;
